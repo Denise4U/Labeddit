f8e70e8389f17fd04e60bae51b6de577a84febe8
Denise Machado Castro <denise.castro1993ii@gmail.com>
2022-09-25 23:26:36 -0300

    logica pag de Erro completa

--- labeddit/src/pages/Error/ErrorPage.jsx
+++ labeddit/src/pages/Error/ErrorPage.jsx
@@ -1,9 +1,19 @@
-import React from "react"
+import Header from "../../components/Header/Header";
+import { ErrorContainer, ErrorPageContainer, PageContainer } from "./styled";
+import error from "../../assets/imgs/404.jpg"
 
-const ErrorPage = () => {
+function ErrorPage({ rightButtonText, setRightButtonText }) {
     return (
-        <h1>ERROR</h1>
-    )
+        <PageContainer>
+            <ErrorPageContainer>
+                <Header rightButtonText={rightButtonText} setRightButtonText={setRightButtonText} showLeftButton isDetails />
+                <ErrorContainer>
+                    <img src={error} alt="Imagem de erro 404" />
+                    <h2>Página não encontrada, aperte no botão de voltar</h2>
+                </ErrorContainer>
+            </ErrorPageContainer>
+        </PageContainer>
+    );
 }
 
-export default ErrorPage
+export default ErrorPage;
